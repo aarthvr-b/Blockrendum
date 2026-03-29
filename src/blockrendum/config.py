5a520571fa63d5e54ef8e09b9f27cfc242b55b1a
aarthvr-b <assefarthur@gmail.com>
2026-03-30 00:00:35 +0200

    changed api_prefix to "/api/v1" instead of empty

--- src/blockrendum/config.py
+++ src/blockrendum/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name:str = "Blockrendum"
     app_env:str = ""
-    api_prefix:str = ""
+    api_prefix:str = "/api/v1"
 
 def get_settings() -> Settings:
     return Settings()
